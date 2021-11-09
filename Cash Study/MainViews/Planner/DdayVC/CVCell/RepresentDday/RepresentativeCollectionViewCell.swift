635aece3ef2a9de897152166da76e84ec5ef5be9
Jiseok97 <wltjr1426@naver.com>
2021-11-10 00:52:32 +0900

    Designed representative dday cell & change layout of height to CV

--- Cash Study/MainViews/Planner/DdayVC/CVCell/RepresentDday/RepresentativeCollectionViewCell.swift
+++ Cash Study/MainViews/Planner/DdayVC/CVCell/RepresentDday/RepresentativeCollectionViewCell.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class RepresentativeCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var dDayNameLbl: UILabel!
+    @IBOutlet weak var dDayLbl: UILabel!
+    @IBOutlet weak var timeLbl: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Cash Study/MainViews/Planner/DdayVC/DdayPageViewController.swift
+++ Cash Study/MainViews/Planner/DdayVC/DdayPageViewController.swift
@@ -24,6 +24,7 @@ class DdayPageViewController: UIViewController {
         
         dDayCV.backgroundColor = UIColor.mainNavy.withAlphaComponent(0.0)
         dDayCV.register(UINib(nibName: "DdayListCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DdayCell")
+        dDayCV.register(UINib(nibName: "RepresentativeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "RepresentativeCell")
         
         if let collectionViewLayout = dDayCV.collectionViewLayout as? UICollectionViewFlowLayout {
             collectionViewLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
@@ -49,10 +50,12 @@ extension DdayPageViewController : UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        if indexPath.row == 1 {
+        if indexPath.row == 0 {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RepresentativeCell", for: indexPath) as? RepresentativeCollectionViewCell else { return UICollectionViewCell() }
             
+            cell.layer.cornerRadius = 8
             
-            return UICollectionViewCell()
+            return cell
         } else {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DdayCell", for: indexPath) as? DdayListCollectionViewCell else { return UICollectionViewCell() }
             
@@ -67,9 +70,13 @@ extension DdayPageViewController : UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = self.view.frame.width * 0.872
-        let height = self.view.frame.height * 0.15673981191
-        
-        return CGSize(width: width, height: height)
+        if indexPath.row == 0 {
+            let height = self.view.frame.height * 0.23510971786
+            return CGSize(width: width, height: height)
+        } else {
+            let height = self.view.frame.height * 0.15673981191
+            return CGSize(width: width, height: height)
+        }
     }
     
 }
