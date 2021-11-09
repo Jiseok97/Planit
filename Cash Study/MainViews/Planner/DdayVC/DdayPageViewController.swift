340f9bb4420a6a96681fc305a1e45ccce3433a21
Jiseok97 <wltjr1426@naver.com>
2021-11-09 22:48:36 +0900

    Change segment Controller background

--- Cash Study/MainViews/Planner/DdayVC/DdayPageViewController.swift
+++ Cash Study/MainViews/Planner/DdayVC/DdayPageViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class DdayPageViewController: UIViewController {
 
+    @IBOutlet weak var dDayCV: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
--- Cash Study/MainViews/Planner/PlannerViewController.swift
+++ Cash Study/MainViews/Planner/PlannerViewController.swift
@@ -16,21 +16,13 @@ class PlannerViewController: UIViewController {
     let plannerVC = PlannerPageViewController()
     let dDayVC = DdayPageViewController()
     
-    let segmentIndicator: UIView = {
-        let v = UIView()
-        
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = UIColor.mainNavy
-        
-        return v
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
         setSGControl()
         setUp()
+        setGradation()
     }
     
     func setUI() {
@@ -40,31 +32,16 @@ class PlannerViewController: UIViewController {
     }
     
     func setSGControl() {
-//        self.sgController.backgroundColor = .clear
-//        self.sgController.tintColor = .clear
-        
-        self.sgController.backgroundColor = UIColor.white.withAlphaComponent(0.0)
-        
         self.sgController.setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "NotoSansCJKkr-Medium", size: 18)!, NSAttributedString.Key.foregroundColor: UIColor.sgNomalColor], for: .normal)
 
         self.sgController.setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "NotoSansCJKkr-Medium", size: 18)!, NSAttributedString.Key.foregroundColor: UIColor.myGray], for: .selected)
         
-        self.view.addSubview(segmentIndicator)
-        
-        self.segmentIndicator.snp.makeConstraints { (make) in
-            
-            make.top.equalTo(self.sgController.snp.bottom).offset(2)
-            make.height.equalTo(2)
-            make.width.equalTo(self.sgController.frame.width / 3)
-            make.centerX.equalTo(self.sgController.snp.centerX)
-                .dividedBy(self.sgController.numberOfSegments)
-            
-        }
-        
         self.sgController.addTarget(self, action: #selector(indexChanged), for: .valueChanged)
-
-        
+        self.sgController.setBackgroundImage(UIImage(named: "navyImg1"), for: .normal, barMetrics: .default)
+        self.sgController.setDividerImage(UIImage(named: "navyImg2"), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+  
     }
+    
     
     func setUp() {
         addChild(plannerVC)
@@ -89,26 +66,6 @@ class PlannerViewController: UIViewController {
     
     
     @objc func indexChanged(_ sender: UISegmentedControl) {
-        let numberOfSegments = CGFloat(self.sgController.numberOfSegments)
-        let selectedIdx = CGFloat(sender.selectedSegmentIndex)
-        
-        self.segmentIndicator.snp.remakeConstraints { (make) in
-            make.top.equalTo(self.sgController.snp.bottom).offset(3)
-            make.height.equalTo(3)
-            make.width.equalTo(self.sgController.frame.width / 3)
-//            make.width.equalTo(15 + titleCnt * 8)
-            make.centerX.equalTo(self.sgController.snp.centerX).dividedBy(numberOfSegments / CGFloat(3.0 + CGFloat(selectedIdx - 1.0) * 2.0))
-        }
-        
-        UIView.animate(withDuration: 0.5, animations: {
-            self.view.layoutIfNeeded()
-            self.segmentIndicator.transform = CGAffineTransform(scaleX: 1.4, y: 1)
-        }) { (finish) in
-            UIView.animate(withDuration: 0.4, animations: {
-                self.segmentIndicator.transform = CGAffineTransform.identity
-            })
-        }
-        
         self.plannerVC.view.isHidden = true
         self.dDayVC.view.isHidden = true
         
@@ -118,14 +75,6 @@ class PlannerViewController: UIViewController {
         default:
             self.dDayVC.view.isHidden = false
         }
-        
-//        if self.segmentController.selectedSegmentIndex == 0 {
-//            self.eatDealVC.view.isHidden = false
-//        } else if self.segmentController.selectedSegmentIndex == 1 {
-//            self.storyVC.view.isHidden = false
-//        } else {
-//            self.topVC.view.isHidden = false
-//        }
     }
     
     
